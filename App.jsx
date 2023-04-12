0f36b9a4d8aad43da4c77bdd12de1b4d67da24d0
0Murshid0 <125559790+0Murshid0@users.noreply.github.com>
2023-04-12 12:57:39 +0500

    1 file modified

--- App.jsx
+++ App.jsx
@@ -12,12 +12,17 @@ export default function App() {
   const HandleSUB = () => {
     setCounter(Number(inputvalue) - Number(inputvalue2)); //Number is used to convert string into number its js feature when handle add grt value from inputvalue its string we don't work on string so we use number to convert it in numaric
   };
-  const HandleMultiple=()=>{
+  const HandleMultiple = () => {
     setCounter(Number(inputvalue) * Number(inputvalue2));
-  }
-  const HandleDivide =()=>{
+  };
+  const HandleDivide = () => {
     setCounter(Number(inputvalue) / Number(inputvalue2));
-  }
+  };
+  const HandleClear = () => {
+    setCounter('');
+    setInputvalue('');
+    setInputvalue2('');
+  };
   return (
     <>
       <input
@@ -49,11 +54,20 @@ export default function App() {
         </td>
         <td>
           {' '}
-          <button type="button" onClick={HandleMultiple}>*</button>{' '}
+          <button type="button" onClick={HandleMultiple}>
+            *
+          </button>{' '}
         </td>
         <td>
           {' '}
-          <button type="button" onClick={HandleDivide}>/</button>{' '}
+          <button type="button" onClick={HandleDivide}>
+            /
+          </button>{' '}
+        </td>
+        <td>
+          <button type="button" onClick={HandleClear}>
+            Clear
+          </button>
         </td>
         <br />
         <br />
